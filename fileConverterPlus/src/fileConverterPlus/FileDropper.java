baf9e78489951b550d2e587cefd1877cc2b62a7b
GroverJamesHamilton <alexander_smith@live.se>
2025-02-22 13:40:28 +0100

    Added DropTargetListener to JLabel

--- fileConverterPlus/src/fileConverterPlus/FileDropper.java
+++ fileConverterPlus/src/fileConverterPlus/FileDropper.java
@@ -2,64 +2,67 @@ package fileConverterPlus;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetEvent;
+import java.awt.dnd.DropTargetListener;
 
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
-public class FileDropper extends JLabel implements MouseListener{
+public class FileDropper extends JLabel implements DropTargetListener{
 	
 FileDropper(String name) {
 	super(name, SwingConstants.CENTER);	
+	
+	//new DropTarget(this, DnDConstants.ACTION_COPY_OR_MOVE, this);
+	new DropTarget(this, DnDConstants.ACTION_NONE, this);
+	
 	this.setSize(300,170);
 	this.setBackground(new java.awt.Color(240,240,230));
 	this.setOpaque(true);
 	this.setForeground(new java.awt.Color(0,0,0));
 	setFont(new java.awt.Font("Times New Roman", Font.BOLD, 18));
 	
-	this.addMouseListener(this);
 }
 
 boolean inside = false;
 
 @Override
-public void mouseClicked(MouseEvent e) {
+public void dragEnter(DropTargetDragEvent dtde) {
 	// TODO Auto-generated method stub
-	System.out.println("Clicked");
-	
-}
-
-@Override
-public void mousePressed(MouseEvent e) {
-	// TODO Auto-generated method stub
-	
-}
-
-@Override
-public void mouseReleased(MouseEvent e) {
-	// TODO Auto-generated method stub
-	
-}
-
-@Override
-public void mouseEntered(MouseEvent e) {
-	// TODO Auto-generated method stub
-	
 	if(!inside) {
 		inside = true;
-		System.out.println("We entered label");
+		System.out.println("Inside");
 	}
+}
+
+@Override
+public void dragOver(DropTargetDragEvent dtde) {
+	// TODO Auto-generated method stub
 	
 }
 
 @Override
-public void mouseExited(MouseEvent e) {
+public void dropActionChanged(DropTargetDragEvent dtde) {
 	// TODO Auto-generated method stub
-	if(inside) {
-		inside = false;
-		System.out.println("We exited label");
-	}
+	
 }
+
+@Override
+public void dragExit(DropTargetEvent dte) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void drop(DropTargetDropEvent dtde) {
+	// TODO Auto-generated method stub
+	
+}
+
+
 
 }
