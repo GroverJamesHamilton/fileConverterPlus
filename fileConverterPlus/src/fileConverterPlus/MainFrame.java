ada197667895445416aae417074b3d9d28fe9a17
GroverJamesHamilton <alexander_smith@live.se>
2025-03-20 02:27:39 +0100

    Added MenuBar

--- fileConverterPlus/src/fileConverterPlus/MainFrame.java
+++ fileConverterPlus/src/fileConverterPlus/MainFrame.java
@@ -8,11 +8,14 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 
 public class MainFrame extends JFrame {
 
 	JFrame frame = new JFrame();
 	JLabel label = new JLabel("Hello!");
+	private Color glacuous = new Color(96, 130, 182);
 
 	MainFrame(){
 
@@ -22,9 +25,19 @@ public class MainFrame extends JFrame {
 		this.setSize(500,500);
 		this.setVisible(true);
 
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		JMenu editMenu = new JMenu("Edit");
+		JMenu helpMenu = new JMenu("Help");
+		
+		menuBar.add(fileMenu);
+		menuBar.add(editMenu);
+		menuBar.add(helpMenu);
+		this.setJMenuBar(menuBar);
+		
 		ImageIcon image = new ImageIcon("ruler.png");
 		this.setIconImage(image.getImage());
-		this.getContentPane().setBackground(new Color(0, 200, 0));
+		this.getContentPane().setBackground(glacuous);
 		List<File> processedFiles = FileDropper.getFiles();
 		System.out.println(processedFiles.size());
 	}
